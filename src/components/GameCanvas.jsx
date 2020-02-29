2995feabb2a14d067bea7af3913ad7fa7c4a83d9
cat-in-a-box <timoshuk.alexander@gmail.com>
2020-02-29 02:37:03 +0200

    a few visual changes

--- src/components/GameCanvas.jsx
+++ src/components/GameCanvas.jsx
@@ -4,7 +4,8 @@ const GameCanvas = () => {
     return (
         <div className="GameCanvas">
             <div>Food eaten:</div>
-            <div class="score" id="score">0</div>
+            <div className="score" id="score">0</div>
+            <div className="scoreUndertext">You can move the snake with arrow buttons</div>
             <canvas class="gameField" id="gameCanvas" width="810" height="510"/>
         </div>
     )
